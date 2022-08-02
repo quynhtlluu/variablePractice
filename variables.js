f53253987dd57bb06ebeb8ac4af077f8f10d8285
Quynh Luu <quynhtlluu@gmail.com>
2022-08-02 11:15:43 -0700

    Add variables that are assigned to Numbers

--- variables.js
+++ variables.js
@@ -1,3 +1,7 @@
 var customerName = "Jim Smith"
 var occupationTitle = "Customer Service Representative"
 var hairColor = "brown"
+
+var customerAge = 37
+var membershipAccountBalance = 18.56
+var membershipTokens = 500
